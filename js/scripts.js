52767786dc84a32ca423385054d582c1635a1ae3
pantelis <pantfx31@gmail.com>
2020-11-26 21:20:34 +0200

    Bootstrap Assignment Final

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ $(document).docready(function(){
         $("#mycarousel").carousel('cycle');
     });
 });
+
+
+
+$('#myCarousel').carousel({
+    interval: 3000,
+ })
+
+ 
+ 
+//  $(document).docready(function() {
+//     $('#list').onclick(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
+//     $('#grid').onclick(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
+// });
